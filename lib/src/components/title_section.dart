62d94141d06c7519930eeef4920b67dea96bc87e
TaufikAkbar7 <taufik.lol911@gmail.com>
2024-02-27 19:27:47 +0700

    feat: learn statefull widget

--- lib/src/components/title_section.dart
+++ lib/src/components/title_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './favorite_section.dart';
 
 class TitleSection extends StatelessWidget {
   final String name;
@@ -41,11 +42,7 @@ class TitleSection extends StatelessWidget {
             ),
           ),
           /*3*/
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          const Text('41'),
+          const FavoriteWidget(),
         ],
       ),
     );
